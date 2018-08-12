14bdcc5ee464ebe9083ceb853cc3c63ea4c65e4b
Boo <booocj@gmail.com>
2018-08-12 18:15:27 +0100

    Finished
    finished

---
+++ proto.js
@@ -0,0 +1,7 @@
+// let s = document.getElementsByClassName("slides");
+s =document.getElementsByClassName("slides");
+imageSize(s,400,200)
+
+let nowa = new Carusel(s, 0 , 4000);
+nowa.startt();
+nowa.addShadow(s);
--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("kocham lolcie!");
+let imageSize = function(arg,e,f){
+    for(let i=0; i < arg.length; i++){
+        arg[i].style.width = e + "px";
+        arg[i].style.height = f + "px";
+        arg[i].parentElement.style.width = e +"px";
+        arg[i].parentElement.style.height = f + "px";
+    }
+}
+
+let Carusel = function (slides,counter = 0, speed) {
+    this.slides = slides;
+    this.counter = counter;
+    this.speed = speed;
+}
+Carusel.prototype.startt  = function(){
+    for(let i = 0; i < this.slides.length; i++){
+        this.slides[i].style.display = "none";
+    }
+    this.counter++;
+    if(this.counter > this.slides.length) {this.counter = 1;}
+
+    this.slides[this.counter-1].style.display = "block";
+
+    setTimeout(() => {
+        nowa.startt();
+    }, this.speed);
+}
+
+Carusel.prototype.addShadow = function(elem){
+    let parent = elem[0].parentElement;
+    return parent.style.boxShadow = " 0 0 20px #333";
+}
